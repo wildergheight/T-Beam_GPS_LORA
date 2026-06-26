0c318e7a23f73db86fdf7c753ed4a7dda12ede99
dylan.klein2018@gmail.com <dylan.klein2018@gmail.com>
2026-06-26 14:25:15 -0700

    increased auto mode steering angle

--- src/main.cpp
+++ src/main.cpp
@@ -552,10 +552,10 @@ void loop() {
         }
         else if (steering < -0.5) { // RIGHT
             controlData.throttle = -1;
-            controlData.steering = 0.85;
+            controlData.steering = 0.80;
         }
         else if (steering > 0.5) { // LEFT
-            controlData.throttle = -0.85;
+            controlData.throttle = -0.80;
             controlData.steering = 1;
         }
         else if (throttle <= -0.5 && abs(steering <= 0.5)) { // SLOWER (BACK)
